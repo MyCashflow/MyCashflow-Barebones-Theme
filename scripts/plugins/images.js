d2a5335655d9bf90ff359ee476002769f212309b
Jukka Koskinen <jukka@pulse247.info>
2016-08-15 13:33:40 +0300

    Change current product image/variation based on the image/variation selection

--- scripts/plugins/images.js
+++ scripts/plugins/images.js
@@ -10,28 +10,83 @@
 		currentImage: '#CurrentProductImage',
 		thumbnails: '#ProductThumbnails a',
 		caption: '#ProductImageCaption',
+		variationsRadio: '.BuyFormVariationRadio',
+		variationsSelect: '.BuyFormVariationSelect',
+
+		imageToVariation: true,
+		variationToImage: true,
 
 		init: function (config) {
-			window.mzOptions = $.extend(true, {}, config);
-			this.bindEvents();
-			this.run();
+			// Enable MagicZoom for the current image.
+			$(this.currentImage).addClass('MagicZoom');
+
+			// Add MagicZoom attributes to thumbnails.
+			$(this.thumbnails).each($.proxy(function (index, el) {
+				var $thumb = $(el);
+				$thumb.attr('data-zoom-id', this.currentImage.substr(1))
+					.attr('data-image', $thumb.attr('href'));
+			}, this));
+
+			window.mzOptions = $.extend(true, {
+				onZoomReady: $.proxy(function () {
+					this.bindEvents();
+
+					// Set the correct current image based on the form selection.
+					if (this.variationToImage && $(this.variationsSelect).length) {
+						$(this.variationsSelect).find('select').trigger('change');
+					} else if (this.variationToImage && $(this.variationsRadio).length) {
+						$(this.variationsRadio).find('input:checked').trigger('change');
+					}
+				}, this)
+			}, config);
 		},
 
 		bindEvents: function () {
 			$(document).on('click', this.thumbnails, $.proxy(this.onClickThumb, this));
+
+			if (this.imageToVariation && $(this.variationsSelect).length) {
+				$(document).on('change', 'select', this.variationsSelect,
+					$.proxy(this.onChangeSelectVariation, this));
+			} else if (this.imageToVariation && $(this.variationsRadio).length) {
+				$(document).on('change', 'input', this.variationsRadio,
+					$.proxy(this.onChangeRadioVariation, this));
+			}
 		},
 
 		onClickThumb: function (evt) {
-			$(this.caption).text($(evt.currentTarget).attr('title'));
+			var title = $(evt.currentTarget).attr('title');
+			this.setCaption(title);
+
+			if (this.imageToVariation && $(this.variationsSelect).length) {
+				var $select = $(this.variationsSelect).find('select').first();
+				var $option = $select.find('option:contains(' + title + ')');
+				$select.val($option.attr('value'));
+			} else if (this.imageToVariation && $(this.variationsRadio).length) {
+				var $label = $(this.variationsRadio).find('label:contains(' + title + ')');
+				var $radio = $label.find('input');
+				$radio.prop('checked', true);
+			}
 		},
 
-		run: function () {
-			$(this.currentImage).addClass('MagicZoom');
-			$(this.thumbnails).each(function () {
-				var $thumb = $(this);
-				$thumb.attr('data-zoom-id', 'CurrentProductImage')
-					.attr('data-image', $thumb.attr('href'));
-			});
+		onChangeRadioVariation: function (evt) {
+			var label = $(evt.currentTarget).closest('label').text().trim();
+			this.setCurrentImageByText(label);
+		},
+
+		onChangeSelectVariation: function (evt) {
+			var label = $(evt.delegateTarget).find(':selected').text().trim();
+			this.setCurrentImageByText(label);
+		},
+
+		setCaption: function (str) {
+			$(this.caption).text(str);
+		},
+
+		setCurrentImageByText: function (text) {
+			var $thumb = $(this.thumbnails).has('img[alt*="' + text + '"]').first();
+			var index = $thumb.closest('li').index();
+			this.setCaption($thumb.attr('title'));
+			window.MagicZoom.switchTo(this.currentImage.substr(1), index);
 		}
 	};
 
