1f5b79911b59f3870671c82390c04a9d824b1954
sdlabs2 <sdlabs.matti@gmail.com>
2017-05-12 14:54:58 +0300

    Updated languages
    Added translations for SE, DE, FR, ES, PT, RU

--- scripts/locales.js
+++ scripts/locales.js
@@ -7,7 +7,7 @@
 	'use strict';
 
 	var Locales = {
-		en: {
+    en: {
 			close: 'Close',
 			expandImage: 'Expand image',
 			loading: 'Loading...',
@@ -15,11 +15,11 @@
 			updating: 'Updating...',
 			updatingCart: 'Updating cart...',
 			updatingCheckout: 'Updating checkout...',
-			next: 'Next',
+      		next: 'Next',
 			previous: 'Previous'
-		},
+    },
 
-		fi: {
+	fi: {
 			close: 'Sulje',
 			expandImage: 'Näytä suurempana',
 			loading: 'Ladataan...',
@@ -27,15 +27,86 @@
 			updating: 'Päivitetään...',
 			updatingCart: 'Ostoskoria päivitetään...',
 			updatingCheckout: 'Kassaa päivitetään...',
-			next: 'Seuraava',
-			previous: 'Edellinen'
-		},
-
+      		next: 'Seuraava',
+      		previous: 'Edellinen'
+    },
+	
+	se: {
+			close: 'Stäng',
+			expandImage: 'Expandera bilden',
+			loading: 'Laddning...',
+			orderCommentSaved: 'Din kommentar har sparats.',
+			updating: 'Uppdatering...',
+			updatingCart: 'Uppdatering kundvagn...',
+			updatingCheckout: 'Uppdatering kassa...',
+			next: 'Nästa',
+			previous: 'Tidigare'
+	},
+		
+    de: {
+			close: 'Schließen',
+			expandImage: 'Bild vergrößern',
+			loading: 'Laden...',
+			orderCommentSaved: 'Auftragsbezeichnung gespeichert.',
+			updating: 'Aktualisierung...',
+			updatingCart: 'Aktualisierung korn...',
+			updatingCheckout: 'Aktualisierung checkout...',
+      		next: 'Nächste',
+			previous: 'Bisherig'
+    },
+		
+    fr: {
+			close: 'Fermer',
+			expandImage: 'Agrandir l\'image',
+			loading: 'Chargement...',
+			orderCommentSaved: 'Commentaire de commande enregistré.',
+			updating: 'Mise à jour...',
+			updatingCart: 'Mise à jour du panier...',
+			updatingCheckout: 'Mise à jour de la caisse...',
+      		next: 'Prochain',
+			previous: 'Précédent'
+    },
+		
+    es: {
+			close: 'Cerrar',
+			expandImage: 'Ampliar imagen',
+			loading: 'Cargando...',
+			orderCommentSaved: 'Ordenar comentario guardado.',
+			updating: 'Actualizando...',
+			updatingCart: 'Actualización de carrito...',
+			updatingCheckout: 'Actualización de la caja...',
+      		next: 'Siguiente',
+			previous: 'Previou'
+    },
+		
+    pt: {
+			close: 'Fechar',
+			expandImage: 'Expandir imagem',
+			loading: 'Carregando...',
+			orderCommentSaved: 'Comentário de ordem salvo.',
+			updating: 'Atualizando...',
+			updatingCart: 'Atualizando carrinho...',
+			updatingCheckout: 'Atualizando checkout...',
+      		next: 'Seguinte',
+			previous: 'Prévio'
+    },
+		
+    ru: {
+			close: 'закрывать',
+			expandImage: 'Развернуть изображение',
+			loading: 'загрузка...',
+			orderCommentSaved: 'Сохранен комментарий о заказе.',
+			updating: 'обновлять...',
+			updatingCart: 'Обновление корзины...',
+			updatingCheckout: 'Обновление Касса...',
+      		next: 'Следующее',
+			previous: 'предыдущий'
+    },
+		
 		get: function (key) {
-			var locale = window.MCF.locale.toLowerCase();
-			return !!this[locale] ? this[locale][key] : this.en[key];
+			return this[window.MCF.locale.toLowerCase()][key];
 		}
-	};
+  };
 
 	$.extend(true, window, { MCF: { Locales: Locales }});
 })();
