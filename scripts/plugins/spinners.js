d668bc5ed327ba8d0befa5819f6ebecf095b212b
Teemu Kuronen <teemu@pulse247.info>
2019-05-28 16:01:41 +0300

    Add max, min and step attribute support to spinner plugin
    Allows quantity spinner to have max, min and step attributes which will be respected by the spinner plugin.

--- scripts/plugins/spinners.js
+++ scripts/plugins/spinners.js
@@ -7,21 +7,24 @@
 	'use strict';
 
 	var incrTmpl = ['<div class="SpinnerControl">',
-			'<button class="SpinnerButton SpinnerButtonInc" type="button"></button>',
+		'<button class="SpinnerButton SpinnerButtonInc" type="button"></button>',
 		'</div>'].join('');
 
 	var decrTmpl = ['<div class="SpinnerControl">',
-			'<button class="SpinnerButton SpinnerButtonDec" type="button"></button>',
+		'<button class="SpinnerButton SpinnerButtonDec" type="button"></button>',
 		'</div>'].join('');
 
 	var Spinners = {
+		decimalSeparator: ',',
+
 		init: function () {
 			this.wrapInputs($(document.body));
 			this.bindEvents();
 		},
 
 		bindEvents: function () {
-			$(document).on('click.spinner', '.SpinnerButton', this.handleClick);
+			$(document).on('click.spinner', '.SpinnerButton', $.proxy(this.handleClick, this));
+			$(document).on('change', '.SpinnerInput input', $.proxy(this.handleChange, this));
 		},
 
 		unbindEvents: function () {
@@ -46,16 +49,47 @@
 			if ($input.attr('max')) {
 				$newInput.attr('data-max', $input.attr('max'));
 			}
+
+			if ($input.attr('min')) {
+				$newInput.attr('data-min', $input.attr('min'));
+			}
+
+			if ($input.attr('step')) {
+				$newInput.attr('data-step', $input.attr('step'));
+			}
 		},
 
-		handleClick: function () {
-			var $input = $(this).closest('.Spinner').find('input');
-			var max = parseInt($input.attr('data-max')) || null;
-			var value = parseInt($input.val()) || 0;
-			value = $(this).is('.SpinnerButtonInc') ? value + 1 : value - 1;
-			value = value <= 0 ? 0 : value;
-			value = !!max && value > max ? max : value;
-			$input.val(value).trigger('change');
+		handleChange: function (evt) {
+			var $input = $(evt.currentTarget);
+			var $spinner = $input.closest('.Spinner');
+			var $buttonInc = $spinner.find('button.SpinnerButtonInc');
+			var $buttonDec = $spinner.find('button.SpinnerButtonDec');
+			var value = this._parseValue($input.val());
+
+			$buttonInc.prop('disabled', value >= $input.attr('data-max'));
+			$buttonDec.prop('disabled', value <= $input.attr('data-min'));
+		},
+
+		handleClick: function (evt) {
+			var $input = $(evt.currentTarget).closest('.Spinner').find('input');
+			var max = parseFloat($input.attr('data-max')) || null;
+			var min = parseFloat($input.attr('data-min')) || null;
+			var step = parseFloat($input.attr('data-step') || 1);
+			var value = this._parseValue($input.val() || 0);
+
+			value = $(evt.currentTarget).is('.SpinnerButtonInc') ? value + step : value - step;
+			value = !!min && value <= min ? min : value;
+			value = !!max && value >= max ? max : value;
+
+			$input.val(this._formatValue(value)).trigger('change');
+		},
+
+		_formatValue: function (amount) {
+			return parseFloat(amount).toString().replace('.', this.decimalSeparator);
+		},
+
+		_parseValue: function (str) {
+			return parseFloat(str.toString().replace(this.decimalSeparator, '.'));
 		}
 	};
 
