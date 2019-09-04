92e71f4fdc40629346fca747772ee08109c5760e
Heikki Tenhunen <heikki@pulse247.info>
2019-09-04 13:37:54 +0300

    WIP Add product-list filters

---
+++ scripts/plugins/filters.js
@@ -0,0 +1,74 @@
+/**
+ * MyCashflow Default Theme
+ * Copyright (c) 2016 Pulse247 Oy
+ * MIT License <http://opensource.org/licenses/MIT>
+ */
+;(function ($) {
+	'use strict';
+
+	$.fn.initFilterGroups = function() {
+		$(this).each(function(i) {
+			var filterGroup = $(this);
+			filterGroup.children('.FilterGroupName').attr('href', '#Group-' + i);
+			filterGroup.children('.FilterList').attr('id', 'Group-' + i);
+		});
+	};
+	$('.FilterGroup').initFilterGroups();
+
+	$.fn.activeFilterGroups = function() {
+		var active = 'ActiveGroup';
+
+		$(this).each(function() {
+			var filterGroup = $(this);
+			var filterOptions = filterGroup.find('li');
+			if (filterOptions.find(':not(.ApplyFilter)').length !== 0) {
+				filterGroup.addClass(active);
+			}
+		});
+	};
+	$('.FilterGroup').activeFilterGroups();
+
+	$.fn.clearSelected = function() {
+		var active = 'Selected';
+		$(this).each(function() {
+			var filterGroup = $(this);
+			$('li', filterGroup).removeClass(active);
+		});
+	};
+
+	$(document).on('click', '.FilterGroupName', function (evt) {
+		evt.preventDefault();
+		var filterGroup = $(this).parent('.FilterGroup'),
+		filterList = $(this).next('.FilterList');
+		filterGroup.toggleClass('Navigable');
+		filterList.focus();
+	});
+
+	$(document).on('click', '.FilterList a', function (evt) {
+		evt.preventDefault();
+		$(this).closest('li').toggleClass('Selected');
+		location.href = $(this).attr('href');
+	});
+
+	$('.FilterList').on({
+		focusout: function () {
+			$(this).data('timer', setTimeout(function () {
+				$(this).parent('.FilterGroup').removeClass('Navigable').clearSelected();
+			}.bind(this), 0));
+		},
+		focusin: function () {
+			clearTimeout($(this).data('timer'));
+		}
+	});
+
+	$('.FilterGroupName').on({
+		focusout: function () {
+			$(this.hash).data('timer', setTimeout(function () {
+				$(this).parent('.FilterGroup').removeClass('Navigable');
+			}.bind(this.hash), 0));
+		},
+		focusin: function () {
+			clearTimeout($(this.hash).data('timer'));
+		}
+	});
+})(jQuery);
